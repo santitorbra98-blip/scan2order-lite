d7d0750b4425c841a444b711bc302e3ecdf45448
santitorbra98-blip <santitorbra98@gmail.com>
2026-05-09 01:12:49 +0100

    .

--- backend/config/mail.php
+++ backend/config/mail.php
@@ -9,8 +9,8 @@ return [
         'smtp' => [
             'transport'  => 'smtp',
             'host'       => env('MAIL_HOST', 'smtp.gmail.com'),
-            'port'       => env('MAIL_PORT', 587),
-            'encryption' => env('MAIL_ENCRYPTION', 'tls'),
+            'port'       => env('MAIL_PORT', 465),
+            'encryption' => env('MAIL_ENCRYPTION', 'ssl'),
             'username'   => env('MAIL_USERNAME'),
             'password'   => env('MAIL_PASSWORD'),
             'timeout'    => (int) env('MAIL_TIMEOUT', 10),
